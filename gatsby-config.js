4aaf5f571c55aca059c9b926ceffde5f11b90466
im-caelli <im.caelli@gmail.com>
2020-06-27 16:27:26 -0400

    Install and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,39 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'im-caelli.github.io',
+    author: 'Caelli',
+    twitter: 'https://twitter.com/im_caelli',
+    tumblr: 'https://im-caelli.tumblr.com/',
+    instagram: 'https://www.instagram.com/im.caelli/',
+    ao3: 'https://archiveofourown.org/users/im_caelli',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path:  `${__dirname}/src/`
+      }
+    },
+    'gatsby-plugin-sharp', 
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              //maxWidth: 750,
+              linkImagesToOriginal: false,
+            }
+          }
+        ]
+      }
+    }
+  ],
 }
